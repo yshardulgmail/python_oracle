f344b4e7b4f8ce05176d2f0960b5c8414ef7c862
Yogesh Shardul <yogesh@Yogeshs-MacBook-Pro.local>
2023-07-11 18:22:30 +0530

    Accenture exercises - part 1

---
+++ exercise_1.py
@@ -0,0 +1,65 @@
+#Answers to the coding exercises in this URL - 
+#https://www.codingninjas.com/studio/library/30-most-common-accenture-coding-questions
+
+
+"""
+1. Execute the given function.
+def differenceofSum(n.m)
+
+The function takes two integrals m and n as arguments. You are required to obtain the total of all integers ranging between 1 to n (both inclusive) which are not divisible by m. You must also return the distinction between the sum of integers not divisible by m with the sum of integers divisible by m.
+
+Assumption
+
+m > 0 and n > 0
+Sum lies within the integral range 
+
+Example
+Input:
+m = 6
+n = 30
+
+Output:
+285
+"""
+
+def differenceofSum(n, m):
+    sum1 = 0
+    sum2 = 0
+
+    i=1
+    while i <= n: 
+        if (i % m) == 0:
+            print(i)
+            sum1 = sum1 + i
+        else:
+            sum2 = sum2 + i
+        i = i + 1
+
+    print("Difference of sum is - ", sum2 - sum1)
+
+
+def exercise_1():
+    n = int(input("Enter value for n:"))
+    m = int(input("Enter value for m:"))
+
+    if n <= 0 or m <= 0:
+        raise Exception("Please enter non zero positive numbers.")
+    
+    differenceofSum(n, m)
+
+
+def exercise_2():
+    arr = []
+
+    print("Enter array elements seaparated by space:")
+    arr = str(input("E.g. 2 3 4 5 6 7: ")).split(" ")
+    
+    if n <= 0 or m <= 0:
+        raise Exception("Please enter non zero positive numbers.")
+    
+    differenceofSum(n, m)
+
+# exercise_1()
+exercise_2()
+
+
---
+++ exercise_3.py
@@ -0,0 +1,47 @@
+#Answers to the coding exercises in this URL - 
+#https://www.codingninjas.com/studio/library/30-most-common-accenture-coding-questions
+
+
+"""
+3. Write a function to validate if the provided two strings are anagrams or not. 
+If the two strings are anagrams, then return ‘yes’. Otherwise, return ‘no’.
+Input:
+
+Input 1: 1st string
+Input 2: 2nd string
+
+Output:
+(If they are anagrams, the function will return ‘yes’. Otherwise, it will return ‘no’.)
+
+Example
+
+Input 1: Listen
+Input 2: Silent
+
+Output:
+Yes 
+"""
+
+def is_anagram(str1, str2):
+    for ch in str1:
+        # if any of the character from string 1 is not present in string 2 they are not anagrams
+        if ch not in str2:
+            print("No")
+            return
+    
+    print("Yes")
+
+def exercise_3():
+    str1 = input("Enter String1:")
+    str2 = input("Enter String2:")
+
+    # if string lengths are not same they are not anagrams
+    if len(str1) != len(str2):
+        print("No")
+    else:
+        is_anagram(str1, str2)
+    
+
+exercise_3()
+
+
