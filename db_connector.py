9181db8f2d991c4dd87795cfc13d3a2a1bbb2b71
Yogesh Shardul <yogesh@Yogeshs-MacBook-Pro.local>
2023-08-22 23:22:10 +0530

    architecure

--- db_connector.py
+++ db_connector.py
@@ -1,14 +1,4 @@
 import oracledb
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
 
 class DBConnector:
     def __init__(self, username, password, dsn="localhost/xepdb1", port=1521, is_cloud=False) -> None:
